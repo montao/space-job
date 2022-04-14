e3bc0787bd19ceec599564a8fb6237d721bdd11e
Jakob Ruckel <jakob@ruckel.de>
2022-04-14 21:27:34 +0200

    MP: Get Host working

--- Assets/Multiplayer/Scripts/PlayerControllerMP.cs
+++ Assets/Multiplayer/Scripts/PlayerControllerMP.cs
@@ -1,19 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Networking;
+using Unity.Netcode;
 
-public class PlayerControllerMP : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+[RequireComponent(typeof(NetworkObject))]
+public class PlayerControllerMP : NetworkBehaviour {
+
+    private CharacterController controller;
+    private float movementSpeed = 5f;
+    private System.Guid id;
+    private string playerName = "";
+
+    void Start() {
+        controller = GetComponent<CharacterController>();
+        id = System.Guid.NewGuid();
+        playerName = id.ToString();
+
+        Debug.Log("Player " + playerName + " created!");
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void Update() {
+        if (IsClient) {
+            ClientUpdate();
+        }
+    }
+
+    void ClientUpdate() {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        var direction = new Vector3(horizontalInput, 0, verticalInput);
+
+        controller.Move(direction * Time.deltaTime * movementSpeed);
     }
 }
--- Assets/Multiplayer/Scripts/PlayerManager.cs
+++ Assets/Multiplayer/Scripts/PlayerManager.cs
@@ -1,18 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Unity.Netcode;
 
-public class PlayerManager : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+public class PlayerManager : NetworkBehaviour {
+
+    public static PlayerManager Instance;
+
+    private NetworkVariable<int> playerCount =
+            new NetworkVariable<int>();
+
+    public int PlayersInGame {
+        get {
+            return playerCount.Value;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void Awake() {
+        if (Instance == null) {
+            Instance = this;
+        } else {
+            Destroy(this);
+        }
+        DontDestroyOnLoad(this);
+    }
+
+    void Start() {
+        NetworkManager.Singleton.OnClientConnectedCallback +=
+            (id) => {
+                if (IsServer) {
+                    Debug.Log("New player " + id);
+                    playerCount.Value++;
+                }
+            };
+
+        NetworkManager.Singleton.OnClientDisconnectCallback +=
+            (id) => {
+                if (IsServer) {
+                    playerCount.Value--;
+                }
+            };
+    }
+
+    void Update() {
+
     }
 }
