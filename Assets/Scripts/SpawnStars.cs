26e97089874b453b60642843502de21878a646bb
Mielein <marie.trojan@uni-weimar.de>
2022-04-17 01:48:48 +0200

    welp..this sucks..new method of star spawn but fading doesnt work yet, tried it with 'color over lifetime' but somehow nothing changes

--- Assets/Scripts/SpawnStars.cs
+++ Assets/Scripts/SpawnStars.cs
@@ -5,26 +5,45 @@ using UnityEngine;
 public class SpawnStars : MonoBehaviour
 {
     public GameObject prefabStars;
-    private GameObject currentStars;
-    private GameObject newStars;
+    private GameObject PartSys_go;
+    private GameObject newStars_go;
+
+
 
     void Start()
     {
-        currentStars = Instantiate(prefabStars, transform.position, Quaternion.identity) as GameObject;
+        PartSys_go =  Instantiate(prefabStars, transform.position, Quaternion.identity) as GameObject;
     }
 
-    void OnTriggerExit(Collider other)
+
+    IEnumerator Wait()
     {
         
-        newStars = Instantiate(prefabStars, transform.position, Quaternion.identity) as GameObject;
-        newStars.transform.parent = transform;
-        currentStars = other.GetComponent<GameObject>();
-        Destroy(currentStars);
-        currentStars = newStars;  
-        Debug.Log("hi");
+        yield return new WaitForSeconds(2.0f);
+    }   
+    
+    void createNewStars(){
+        
+
+        if(Vector3.Distance(PartSys_go.transform.position, transform.position) > 200){
+
+            //newStars_go = Instantiate(prefabStars, transform.position /* + new Vector3(100f,100f,100f) */, Quaternion.identity) as GameObject;
+            newStars_go = Instantiate(prefabStars, transform.forward , Quaternion.identity) as GameObject;
+            StartCoroutine(Wait());
+            Destroy(PartSys_go);
+            PartSys_go = newStars_go; 
+        } 
+        if(PartSys_go == null){
+            PartSys_go = newStars_go; 
+        }
+            
+
     }
+
+
     void Update()
     {
+        createNewStars();
         
     }
 }
