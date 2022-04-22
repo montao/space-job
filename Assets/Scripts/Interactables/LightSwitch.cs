b1f6f69b2e3fe61b81ff74c98e63619c0ceb882e
fionn-the-goose <ericksoncooperation@gmail.com>
2022-04-22 18:49:37 +0200

    code for switch written, now we test it

---
+++ Assets/Scripts/Interactables/LightSwitch.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Netcode;
+
+public class LightSwitch : NetworkBehaviour
+{
+    private SphereCollider m_SwitchCollider;
+    private NetworkVariable<bool> m_LightStage = new NetworkVariable<bool>(true);
+    private bool m_IsInArea = false;
+    
+    public List<Light> Lights;
+
+    public override void OnNetworkSpawn(){
+        m_LightStage.OnValueChanged += OnStateChange;
+    }
+    public override void OnNetworkDespawn(){
+        m_LightStage.OnValueChanged -= OnStateChange;
+    }
+    [ServerRpc(RequireOwnership = false)]
+    public void SwitchServerRpc(){
+        m_LightStage.Value = !m_LightStage.Value;
+    }
+    public void OnStateChange(bool previous, bool current){
+        SetLightConditions(current);
+    }
+
+    void SetLightConditions(bool on){
+        foreach(var light in Lights){
+            light.gameObject.SetActive(on);
+        }
+    }
+    private void Awake() {
+        SetLightConditions(m_LightStage.Value);
+        m_SwitchCollider = GetComponent<SphereCollider>();
+    }
+    private void OnTriggerEnter(Collider other) {
+        PlayerAvatar player = other.GetComponent<PlayerAvatar>();
+        if(player != null && player.IsOwner){
+            Debug.Log("Local player entered");
+            m_IsInArea = true;
+        }
+    }
+    private void OnTriggerExit(Collider other) {
+        PlayerAvatar player = other.GetComponent<PlayerAvatar>();
+        if(player != null && player.IsOwner){
+            Debug.Log("Local player entered");
+            m_IsInArea = true;
+        }
+    }
+    void Update(){  
+        if(m_IsInArea && Input.GetKeyDown(KeyCode.E)){
+            SwitchServerRpc();
+        }    
+    }
+}
