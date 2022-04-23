a0f01c2f90bad755d21867e721ccdc10a236c7d8
Jakob Ruckel <jakob@ruckel.de>
2022-04-23 21:33:03 +0200

    Messing around w/ Cinemachine for cam swap

--- Assets/Scripts/CameraSwap.cs
+++ Assets/Scripts/CameraSwap.cs
@@ -1,7 +1,22 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
 
-public class CameraSwap : MonoBehaviour
-{
+public class CameraSwap : MonoBehaviour {
+    private CinemachineVirtualCamera m_camera;
+
+    void Start() {
+        //m_camera = GetComponentInChildren<CinemachineVirtualCamera>();
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        var player = other.GetComponent<PlayerAvatar>();
+        //if (player != null && player.IsOwner && Camera.main != m_camera) {
+            
+        //}
+    }
+    private void OnTriggerExit(Collider other) {
+        var player = other.GetComponent<PlayerAvatar>();
+        //if (player != null && player.IsOwner && Camera.main != m_camera) {
+        //}
+    }
 }
