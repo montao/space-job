25d91a03a0a225fdabbb584e6727392f44c7c766
Jakob Ruckel <jakob@ruckel.de>
2022-04-23 21:38:29 +0200

    Goodbye, CameraSwap

--- Assets/Scripts/CameraSwap.cs
+++ Assets/Scripts/CameraSwap.cs
@@ -4,21 +4,4 @@ using UnityEngine;
 
 public class CameraSwap : MonoBehaviour
 {
-    private Camera activeCamera;
-    public Camera nextCamera;
-    private Collider activeArea;
-
-    void Start()
-    {
-        activeArea = GetComponent<Collider>();
-        activeCamera = GetComponentInChildren<Camera>();
-    }
-
-    private void OnTriggerExit(Collider other) {
-        Debug.Log("mmmh");
-        if(other.GetComponent<CharacterController>() != null)
-            Debug.Log("charakter");
-            activeCamera.enabled = false;
-            nextCamera.enabled = true;
-    }
 }
