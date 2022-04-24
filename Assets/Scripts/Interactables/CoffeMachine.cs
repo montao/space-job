3bcba73f93960deaa37c81c85202c28b28293d00
jreeee <jeremias.michael.kilian@uni-weimar.de>
2022-04-24 21:42:17 +0200

    one machine one cup

--- Assets/Scripts/Interactables/CoffeMachine.cs
+++ Assets/Scripts/Interactables/CoffeMachine.cs
@@ -7,6 +7,9 @@ public class CoffeMachine : Interactable<bool> {
     public GameObject machine;
    
     protected override void Interaction(){
+        GameObject freshCup = Instantiate(cupPrefab, machine.GetComponent<Transform>().position, Quaternion.identity);
+        freshCup.GetComponent<NetworkObject>().Spawn();
+        Debug.Log("new cup");
         SetServerRpc(!Value);
     }
 
@@ -16,9 +19,6 @@ public class CoffeMachine : Interactable<bool> {
     [ServerRpc(RequireOwnership = false)]
     public void SetServerRpc(bool value){
         m_State.Value = value;
-        GameObject freshCup = Instantiate(cupPrefab, machine.GetComponent<Transform>().position, Quaternion.identity);
-        freshCup.GetComponent<NetworkObject>().Spawn();
-        Debug.Log("new cup");
     }
     private void Awake() {
     }
