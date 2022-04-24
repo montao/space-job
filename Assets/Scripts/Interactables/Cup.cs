ed28d5a6b0bd2354b20428ac9bed0189dd772acc
Jakob Ruckel <jakob@ruckel.de>
2022-04-24 13:08:48 +0200

    Try out NetworkTransform'd cup

--- Assets/Scripts/Interactables/Cup.cs
+++ Assets/Scripts/Interactables/Cup.cs
@@ -8,57 +8,39 @@ using Unity.Netcode;
   Feckn delicious shit, oh no..
   */
 
-[System.Serializable]
-public struct Itemstate{
-    public Vector3 location;
-    public Quaternion rotation;
-    public bool inWorld;
-}
-public class Cup : Interactable<Itemstate>{
+public class Cup : Interactable<bool>{
     private MeshRenderer m_Mesh;
     private Rigidbody m_Rigidbody;
     private List<Collider> m_AllCollider;
+
     protected override void Interaction(){
-        Itemstate newState = Value;
-        newState.inWorld = !newState.inWorld;
-        SetServerRpc(newState);
+        SetServerRpc(!Value);
     }
-    public override void OnStateChange(Itemstate previous, Itemstate current){
-        if (current.inWorld != previous.inWorld) {
+
+    public override void OnStateChange(bool previous, bool current){
+        if (current != previous) {
             // inWorld changed, i.e. item was dropped or
             // picked up
-            UpdateWorldstate(current.inWorld);
-        }
-        if (current.inWorld && !IsOwner) {
-            transform.position = current.location;
-            transform.rotation = current.rotation;
+            UpdateWorldstate(current);
         }
     }
 
     [ServerRpc(RequireOwnership = false)]
-    public void SetServerRpc(Itemstate value){
+    public void SetServerRpc(bool value){
         m_State.Value = value;
     }
+
     private void Awake() {
         m_AllCollider = new List<Collider>(GetComponents<Collider>());
         m_Mesh = GetComponent<MeshRenderer>();
         m_Rigidbody = GetComponent<Rigidbody>();
     }
+
     public void UpdateWorldstate(bool inWorld){
         foreach(Collider colli in m_AllCollider){
             colli.enabled = inWorld;
         }
         m_Mesh.enabled = inWorld;
         m_Rigidbody.isKinematic = !inWorld && !IsOwner; // TODO does this work? 
-    }
-
-    public override void Update() {
-        base.Update();
-        if (IsOwner && Value.inWorld) {
-            Itemstate newState = Value;
-            newState.location = transform.position;
-            newState.rotation = transform.rotation;
-            SetServerRpc(Value);
-        }
     }
 }
