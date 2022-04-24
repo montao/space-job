e4c4970a51fc2802f4f6a6563d6b5de4e1d44a03
jreeee <jeremias.michael.kilian@uni-weimar.de>
2022-04-24 21:57:50 +0200

    fixed non-server spawns

--- Assets/Scripts/Interactables/CoffeMachine.cs
+++ Assets/Scripts/Interactables/CoffeMachine.cs
@@ -7,18 +7,18 @@ public class CoffeMachine : Interactable<bool> {
     public GameObject machine;
    
     protected override void Interaction(){
-        GameObject freshCup = Instantiate(cupPrefab, machine.GetComponent<Transform>().position, Quaternion.identity);
-        freshCup.GetComponent<NetworkObject>().Spawn();
-        Debug.Log("new cup");
         SetServerRpc(!Value);
     }
 
     public override void OnStateChange(bool previous, bool current) {
-        SetServerRpc(current);
     }
     [ServerRpc(RequireOwnership = false)]
     public void SetServerRpc(bool value){
         m_State.Value = value;
+
+        GameObject freshCup = Instantiate(cupPrefab, machine.GetComponent<Transform>().position, Quaternion.identity);
+        freshCup.GetComponent<NetworkObject>().Spawn();
+        Debug.Log("new cup");
     }
     private void Awake() {
     }
