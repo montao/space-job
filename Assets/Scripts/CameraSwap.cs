8726f52c9e660bc51cf6b109b10f8deef0cd86ce
fionn-the-goose <ericksoncooperation@gmail.com>
2022-04-27 16:18:19 +0200

    cinemashine for in room zoom swap

--- Assets/Scripts/CameraSwap.cs
+++ Assets/Scripts/CameraSwap.cs
@@ -3,17 +3,27 @@ using Cinemachine;
 
 public class CameraSwap : MonoBehaviour {
     private CinemachineVirtualCamera m_Camera;
+    public bool InRoom;
 
     void Start() {
-        m_Camera = GetComponentInChildren<CinemachineVirtualCamera>();
-        LookAtPlayer();
+        m_Camera = GetComponent<CinemachineVirtualCamera>();
+        if(m_Camera == null){
+            m_Camera = GetComponentInChildren<CinemachineVirtualCamera>();
+        }
+        if(!InRoom){
+            LookAtPlayer();
+        }
     }
 
     private void OnTriggerEnter(Collider other) {
         var player = other.GetComponent<PlayerAvatar>();
-
         if (player != null && player.IsOwner && CameraBrain.Instance.ActiveCameraObject != m_Camera.VirtualCameraGameObject) {
-            m_Camera.Priority = 20;
+            if (InRoom){
+                m_Camera.Priority = 30;
+            }
+            else{
+                m_Camera.Priority = 20;
+            }
         }
     }
     private void OnTriggerExit(Collider other) {
