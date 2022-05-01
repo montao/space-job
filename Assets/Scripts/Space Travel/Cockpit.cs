636a0c2ac5384740e462764b25880e37b96f3982
Mielein <marie.trojan@uni-weimar.de>
2022-05-01 17:36:10 +0200

    cockpitview

--- Assets/Scripts/Space Travel/Cockpit.cs
+++ Assets/Scripts/Space Travel/Cockpit.cs
@@ -9,39 +9,34 @@ public class Cockpit : Interactable<bool> {
     public CinemachineVirtualCamera cam;
     public int cam_prio;
     public GameObject stars;
+
     
 
     protected override void Interaction(){
         SetServerRpc(!Value);
+        SetCockConditions(!Value);
     }
 
+    void SetCockConditions(bool on){
+        cam.Priority = cam_prio;
+        foreach (var player in FindObjectsOfType<PersistentPlayer>()) {
+            Debug.Log(player.Avatar.name);
+            player.Avatar.GetComponent<CharacterController>().enabled = on;
+        }
+        stars.GetComponent<Movnt>().enabled = on;
+        if(on){
+            cam.Priority = 0;
+        }
+    }
     public override void OnStateChange(bool previous, bool current){
+        SetCockConditions(previous);
     }
     [ServerRpc(RequireOwnership = false)]
 
     public void SetServerRpc(bool value){
         m_State.Value = value;
-        cam.Priority = cam_prio;
-        foreach (var player in FindObjectsOfType<PersistentPlayer>()) {
-            Debug.Log(player.Avatar.name);
-            player.Avatar.GetComponent<CharacterController>().enabled = false;
-        }
-        stars.GetComponent<Movnt>().enabled = true;
         
         
-    }
-    /* void Update(){
-        if(Input.GetKeyDown(KeyCode.E)){
-            cam_prio = 0;
-            foreach (var player in FindObjectsOfType<PersistentPlayer>()) {
-                Debug.Log(player.Avatar.name);
-                player.Avatar.GetComponent<CharacterController>().enabled = true;
-            }
-            stars.GetComponent<Movnt>().enabled = false;
-        }
-    } */
-    private void Awake() {
-
     }
 
 }
