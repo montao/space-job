15525b5234bb938558f9f9f681a1601237f743af
YoFionn <ericksoncooperation@gmail.com>
2022-05-03 17:44:33 +0200

    holo fix maybe testing in multiplayer

--- Assets/Scripts/HologramAnimation.cs
+++ Assets/Scripts/HologramAnimation.cs
@@ -4,25 +4,29 @@ using Unity.Netcode;
 public class HologramAnimation : NetworkBehaviour
 {
     public int ActiveAnimation;
-    private NetworkVariable<int> m_Animation = new NetworkVariable<int>(2); 
+    private NetworkVariable<int> m_Animation = new NetworkVariable<int>(-1); 
     private Animator m_HoloAnimator;
     
     public override void OnNetworkSpawn(){
         m_Animation.OnValueChanged += OnStateChange;
+        m_Animation.Value = ActiveAnimation;
     }
     public override void OnNetworkDespawn(){
         m_Animation.OnValueChanged -= OnStateChange;
     }
     public void OnStateChange(int previous, int current){
         if (m_HoloAnimator != null) {
-            m_HoloAnimator.SetInteger("active", current);
+            Debug.Log("old State:" + current);
+            Debug.Log("new State:" + current);
+            //m_HoloAnimator.SetInteger("active", current);
         }
     }
 
+    private void Update() {
+        m_HoloAnimator.SetInteger("active", m_Animation.Value);
+    }
+
     private void Start() {
-        if (IsServer) {
-            m_Animation.Value = ActiveAnimation;
-        }
         m_HoloAnimator = GetComponent<Animator>();
         if (!m_HoloAnimator) {
             Debug.LogWarning("No Animator component found on Hologram " + gameObject.name);
--- misc/animation-Dictionary.md
+++ misc/animation-Dictionary.md
@@ -4,3 +4,6 @@
 - 1: walk
 - 2: interact
 - 3: armwave dance
+- 4: jumping jacks
+- 5: sitting down
+- 6: drinking
