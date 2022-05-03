4a07103d07f0c3c793edbbfaaaa7d76c7f2fa2fd
Mielein <marie.trojan@uni-weimar.de>
2022-05-03 17:32:50 +0200

    how do networks work

--- Assets/Scripts/Space Travel/Movnt.cs
+++ Assets/Scripts/Space Travel/Movnt.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Unity.Netcode;
+using Unity.Netcode.Samples;
 
-public class Movnt : MonoBehaviour
-{
+[RequireComponent(typeof(ClientNetworkTransform))]
+public class Movnt : NetworkBehaviour {
     private Vector3 lastMousePos = new Vector3(255, 255, 255);
     float sensitivity = 0.25f;
+
     
 
     void Update(){
