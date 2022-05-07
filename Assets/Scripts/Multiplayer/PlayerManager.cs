d07a17ebe8646d8e56ca9ebb186a799b5bfff1b9
Mielein <marie.trojan@uni-weimar.de>
2022-05-07 18:14:18 +0200

    this must be it

--- Assets/Scripts/Multiplayer/PlayerManager.cs
+++ Assets/Scripts/Multiplayer/PlayerManager.cs
@@ -27,7 +27,7 @@ public class PlayerManager : NetworkBehaviour {
             string info = _playerCount.Value + "/4: ";
             int n = 0;
             foreach (var player in _players) {
-                info = _playerCount.Value + "/4: " /* info + ((n++ == 0) ? "" : "> ") */ + player.PlayerName + " just joined! \n";
+                info =  info  + player.PlayerName + " just joined! \n";
             }
             return info;
         }
