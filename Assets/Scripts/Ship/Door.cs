837b187cfa21e79e041790af114f42d647fa1397
YoFionn <ericksoncooperation@gmail.com>
2022-05-08 14:00:31 +0200

    the door has a sweet littel animation now, quiet nice

--- Assets/Scripts/Ship/Door.cs
+++ Assets/Scripts/Ship/Door.cs
@@ -7,6 +7,7 @@ public class Door : NetworkBehaviour {
     private NetworkVariable<bool> m_Open = new NetworkVariable<bool>(true);
     private Collider m_PhysicalDoor;
     private MeshRenderer m_PsychologicalDoor;
+    private Animator m_DoorAnimator;
 
 
     public void SetRoom(Room room) {
@@ -32,14 +33,16 @@ public class Door : NetworkBehaviour {
     }
 
     public void OnDoorStateChange(bool previous, bool current){
-        m_PsychologicalDoor.enabled = current;
-        m_PhysicalDoor.enabled = current;
+        m_DoorAnimator.SetBool("open", current);
+        //m_PsychologicalDoor.enabled = current;
+        //m_PhysicalDoor.enabled = current;
     }
 
     // Start is called before the first frame update
     void Start() {
         m_PhysicalDoor = GetComponent<Collider>();
         m_PsychologicalDoor = GetComponent<MeshRenderer>();
+        m_DoorAnimator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
