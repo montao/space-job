9446381aa7b30450c12d3e54912b716f8f28e576
Jakob Ruckel <jakob@ruckel.de>
2022-05-09 21:53:04 +0200

    Prepare Navigation Terminal

---
+++ Assets/Scripts/Interactables/NavTerminal.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NavTerminal : Interactable<bool> {
+
+    protected override void Interaction() {
+
+    }
+
+    public override void OnStateChange(bool prev, bool current) {
+
+    }
+}
