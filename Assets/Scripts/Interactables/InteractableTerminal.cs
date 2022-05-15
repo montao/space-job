04fd2e57a253e5006d719e77b8b6caac19d40169
fionn-the-goose <ericksoncooperation@gmail.com>
2022-05-15 17:08:49 +0200

    some minor changes in script an scene

--- Assets/Scripts/Interactables/InteractableTerminal.cs
+++ Assets/Scripts/Interactables/InteractableTerminal.cs
@@ -13,6 +13,7 @@ public class InteractableTerminal : InteractableBase
         m_CameraSwap = GetComponent<CameraSwap>();
         m_TargetCanvas = GetComponentInChildren<Canvas>();
         Display.transform.SetParent(m_TargetCanvas.transform, false);
+        
     }
     // Start is called before the first frame update
     protected override void Interaction(){
