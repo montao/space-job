4a5f30f9079e454b918939720e326d87144a130b
fionn-the-goose <ericksoncooperation@gmail.com>
2022-05-15 17:02:09 +0200

    wie soll ich denn mithalten mit den commits wenn ich das commiten vernachlässige

--- Assets/Scripts/Interactables/InteractableTerminal.cs
+++ Assets/Scripts/Interactables/InteractableTerminal.cs
@@ -4,14 +4,29 @@ using UnityEngine;
 
 public class InteractableTerminal : InteractableBase
 {
-    public TerminalCanvas display;
+    public TerminalCanvas Display;
     private Canvas m_TargetCanvas;
+    private CameraSwap m_CameraSwap;
+    private bool m_LocalPlayerIsInteracting = false;
+
     private void Awake() {
+        m_CameraSwap = GetComponent<CameraSwap>();
         m_TargetCanvas = GetComponentInChildren<Canvas>();
-        display.transform.SetParent(m_TargetCanvas.transform, false);
+        Display.transform.SetParent(m_TargetCanvas.transform, false);
     }
     // Start is called before the first frame update
     protected override void Interaction(){
-
+        m_LocalPlayerIsInteracting = !m_LocalPlayerIsInteracting;
+        Display.SetInteractableEnable(m_LocalPlayerIsInteracting);
+        if (m_LocalPlayerIsInteracting) {
+            m_CameraSwap.SwitchTo();
+            if (!ShipManager.Instance.HasPower) {
+                // HCI:  Only lock movement when we can interact with the terminal
+                PlayerManager.Instance.LocalPlayer.Avatar.LockMovement(GetHashCode());
+            }
+        } else {
+            PlayerManager.Instance.LocalPlayer.Avatar.ReleaseMovementLock(GetHashCode());
+            m_CameraSwap.SwitchAway();
+        }
     }
 }
