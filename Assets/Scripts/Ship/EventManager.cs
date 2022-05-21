c5ce13431483fc922c63686011ee82ec66152642
fionn-the-goose <ericksoncooperation@gmail.com>
2022-05-21 19:10:45 +0200

    space is risc free at the moment please hold the line

---
+++ Assets/Scripts/Ship/EventManager.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public enum Event {
+    POWER_OUTAGE,
+    COSMIC_HORROR,
+    HULL_BREACH,
+    NONE,
+}
+
+public class EventManager : MonoBehaviour{
+    
+}
--- Assets/Scripts/Ship/Map.cs
+++ Assets/Scripts/Ship/Map.cs
@@ -1,13 +1,6 @@
 using UnityEngine;
 
-public enum Event {
-    POWER_OUTAGE,
-    COSMIC_HORROR,
-    NONE,
-}
-
 public struct MapState {
-    public Event ev;  // green
     public float risk;  // red
     // TODO Biomes (blue)
 }
@@ -26,11 +19,9 @@ public class Map : MonoBehaviour {
     public MapState GetState(Vector2 pos) {
         MapState state = new MapState();
         if (pos.x < 0 || pos.y < 0 || pos.x > 1024 || pos.y > 1024) {
-            state.ev = Event.COSMIC_HORROR;
             state.risk = 1;
         } else {
             Color color = MapTexture.GetPixel(Mathf.RoundToInt(pos.x), Mathf.RoundToInt(pos.y));
-            state.ev = color.g > 0.5 ? Event.POWER_OUTAGE : Event.NONE;
             state.risk = color.r;
         }
         return state;
@@ -42,9 +33,6 @@ public class Map : MonoBehaviour {
                 var state = GetState(new Vector2(x, y));
                 float risk = (int)(state.risk*8)/8.0f;
                 Color col = new Color(0.2f, risk, 0.2f);
-                if (state.ev == Event.POWER_OUTAGE) {
-                    col.r = 1f;
-                }
                 IngameMapTexture.SetPixel(x, y, col);
             }
         }
