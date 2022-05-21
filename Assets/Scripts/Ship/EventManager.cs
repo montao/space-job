bc26f256ea51daff6b7e1c3dce4bc78ccdad8602
fionn-the-goose <ericksoncooperation@gmail.com>
2022-05-21 20:14:51 +0200

    riskibuiscuit output

--- Assets/Scripts/Ship/EventManager.cs
+++ Assets/Scripts/Ship/EventManager.cs
@@ -10,6 +10,9 @@ public enum Event {
 public class EventManager : MonoBehaviour{
     [SerializeField]
     private Map m_Map;
+    [SerializeField]
+    [Range(0,1)]
+    private float risk;
 
     public void DiceRoll(){
         MapState state = m_Map.GetState(ShipManager.Instance.GetShipPosition());
@@ -18,8 +21,8 @@ public class EventManager : MonoBehaviour{
         }        
         
         float hull_breach_dice = Random.value;
-
-        float hull_breach_risk;
-
+        float ship_speed = ShipManager.Instance.GetShipSpeed()/ShipSteering.MAX_TRANSLATION_VELOCITY;
+        float hull_breach_risk = (0.3f*Mathf.Atan(4.3f*(ship_speed-0.4f))+0.5f);
+        Debug.Log("ship_speed: " + ship_speed + ", riskybiskuit: " + hull_breach_risk);
     }
 }
