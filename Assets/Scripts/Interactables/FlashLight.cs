17b8b7075f55877eac91a712ad49325ac6d12e1b
YoFionn <ericksoncooperation@gmail.com>
2022-05-22 14:01:20 +0200

    spotlight will not be that easy.. need to be held by the player

--- Assets/Scripts/Interactables/FlashLight.cs
+++ Assets/Scripts/Interactables/FlashLight.cs
@@ -1,18 +1,37 @@
-using System.Collections;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
+using Unity.Netcode;
+using Unity.Netcode.Components;
 
-public class FlashLight : MonoBehaviour
+public class FlashLight : DroppableInteractable
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+
+    private Light m_Beam;
+    // maybe needs to be network variable later
+    private bool m_TurnedOn = true;
+// this would be always the same, so maybe it can be shorten?
+//----------------------------------------------------------------------------------------------
+    private void Awake() {
+        m_AllCollider = new List<Collider>(GetComponentsInParent<Collider>());
+        m_AllCollider.AddRange(GetComponents<Collider>());
+        m_Mesh = GetComponentInParent<MeshRenderer>();
+        m_Rigidbody = GetComponentInParent<Rigidbody>();
+        m_NetTransform = GetComponentInParent<NetworkTransform>();
+        m_Beam = GetComponent<Light>();
     }
 
-    // Update is called once per frame
-    void Update()
+    protected override void Interaction()
     {
-        
+        base.Interaction();
+        SetServerRpc((int) NetworkManager.Singleton.LocalClientId);
+    }
+    
+//----------------------------------------------------------------------------------------------
+    public override int SelfInteraction(PlayerAvatar avatar)
+    {
+        m_TurnedOn = !m_TurnedOn;
+        m_Beam.enabled = m_TurnedOn;
+        return 2;
     }
 }
