3e59745c66a21cf664a4fe4427a6c3dfc35b4412
Jakob Ruckel <jakob@ruckel.de>
2022-05-24 16:38:59 +0200

    Some UI for ship control

--- Assets/Scripts/UI/NavTerminalConsole.cs
+++ Assets/Scripts/UI/NavTerminalConsole.cs
@@ -9,10 +9,24 @@ public class NavTerminalConsole : MonoBehaviour {
     [SerializeField]
     private SevenSegmentDisplay m_SpeedDisp;
 
+    [SerializeField]
+    private SpriteRenderer m_SpriteRendererFwd;
+    [SerializeField]
+    private SpriteRenderer m_SpriteRendererRev;
+    [SerializeField]
+    private SpriteRenderer m_SpriteRendererLeft;
+    [SerializeField]
+    private SpriteRenderer m_SpriteRendererRight;
+
     // Update is called once per frame
     void Update() {
         m_AngleDisp.DisplayNumber(Mathf.Repeat(ShipManager.Instance.GetShipAngle(), 360f), 1);
         m_TargetSpeedDisp.DisplayNumber(ShipManager.Instance.GetTargetShipSpeed(), 1);
         m_SpeedDisp.DisplayNumber(ShipManager.Instance.GetShipSpeed(), 1);
+
+        m_SpriteRendererFwd.enabled = ShipManager.Instance.Steering.GetThrusterState(ShipSteering.Thruster.TRANSLATE_FORWARD);
+        m_SpriteRendererRev.enabled = ShipManager.Instance.Steering.GetThrusterState(ShipSteering.Thruster.TRANSLATE_BACKWARD);
+        m_SpriteRendererLeft.enabled = ShipManager.Instance.Steering.GetThrusterState(ShipSteering.Thruster.ROTATE_LEFT);
+        m_SpriteRendererRight.enabled = ShipManager.Instance.Steering.GetThrusterState(ShipSteering.Thruster.ROTATE_RIGHT);
     }
 }
