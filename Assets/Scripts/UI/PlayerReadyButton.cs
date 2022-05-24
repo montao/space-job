b07a91946d86cef007a10f33cd7c92f27a3b4432
Mielein <marie.trojan@uni-weimar.de>
2022-05-24 20:06:47 +0200

    small change

--- Assets/Scripts/UI/PlayerReadyButton.cs
+++ Assets/Scripts/UI/PlayerReadyButton.cs
@@ -8,37 +8,37 @@ using TMPro;
 
 public class PlayerReadyButton : Interactable<bool> {
     PlayerAvatar player;
-    private int readyCounter = 0;
     private bool m_LocalPlayerInteracting = false;
 
     protected override void Interaction(){
         m_LocalPlayerInteracting = !m_LocalPlayerInteracting;
         SetServerRpc(m_LocalPlayerInteracting);
         SetPlayerConditions(m_LocalPlayerInteracting); 
-        SetReadyConditions(Value);
+        SetReadyConditions(m_LocalPlayerInteracting);
 
     }
     void SetPlayerConditions(bool on){
         PlayerManager.Instance.LocalPlayer.Avatar.ready.Value = on;   
         
-        /* if(readyCounter == PlayerManager.Instance.Players.Count)  NetworkManager.Singleton.SceneManager.LoadScene("SampleScene", LoadSceneMode.Single); */
+        /* if(readyCouter == PlayerManager.Instance.Players.Count)  NetworkManager.Singleton.SceneManager.LoadScene("SampleScene", LoadSceneMode.Single); */
 
     }
     void SetReadyConditions(bool ready){
+        int readyCouter = 0;
         foreach (var player in FindObjectsOfType<PersistentPlayer>()) {
             if(IsOwner){
                 if (PlayerManager.Instance.LocalPlayer.Avatar.ready.Value) {
-                    readyCounter++;
+                    readyCouter++;
                
                 }
             }
             
         } 
-        Debug.Log("ready counter: " + readyCounter + " Player Counter: " + PlayerManager.Instance.Players.Count);
+        Debug.Log("ready counter: " + readyCouter + " Player Counter: " + PlayerManager.Instance.Players.Count);
     }
 
     void FixedUpdate() {
-        if(readyCounter >= PlayerManager.Instance.Players.Count)  NetworkManager.Singleton.SceneManager.LoadScene("SampleScene", LoadSceneMode.Single);
+        if(readyCouter >= PlayerManager.Instance.Players.Count)  NetworkManager.Singleton.SceneManager.LoadScene("SampleScene", LoadSceneMode.Single);
     }
     public override void OnStateChange(bool previous, bool current){
         //SetPlayerConditions(current);
