7fad7dde4719a393d3f3a0d31c307ebb92f4a84b
Mielein <marie.trojan@uni-weimar.de>
2022-05-24 14:31:45 +0200

    changed the the terminal for only choosing character, added lightswitch for changing ready status, character doesnt stay on scene change

---
+++ Assets/Scripts/UI/PlayerReadyButton.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Netcode;
+using Cinemachine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class PlayerReadyButton : Interactable<bool> {
+    PlayerAvatar player;
+    private bool m_LocalPlayerInteracting = false;
+    int readyCouter = 0;
+
+    protected override void Interaction(){
+        m_LocalPlayerInteracting = !m_LocalPlayerInteracting;
+        SetServerRpc(m_LocalPlayerInteracting);
+        SetPlayerConditions(m_LocalPlayerInteracting); 
+        readyCouter++;
+
+    }
+    void SetPlayerConditions(bool on){
+        PlayerManager.Instance.LocalPlayer.Avatar.ready.Value = on;   
+        
+        /* if(readyCouter == PlayerManager.Instance.Players.Count)  NetworkManager.Singleton.SceneManager.LoadScene("SampleScene", LoadSceneMode.Single); */
+
+    }
+    void SetReadyConditions(bool ready){
+        int readyCouter = 0;
+        foreach (var player in FindObjectsOfType<PersistentPlayer>()) {
+            if(IsOwner){
+                if (PlayerManager.Instance.LocalPlayer.Avatar.ready.Value) {
+                    readyCouter++;
+               
+                }
+            }
+            
+        }
+        Debug.Log("ready counter: " + readyCouter + " Player Counter: " + PlayerManager.Instance.Players.Count);
+    }
+
+    void FixedUpdate() {
+        if(readyCouter >= PlayerManager.Instance.Players.Count)  NetworkManager.Singleton.SceneManager.LoadScene("SampleScene", LoadSceneMode.Single);
+    }
+    public override void OnStateChange(bool previous, bool current){
+        //SetPlayerConditions(current);
+    }
+
+    [ServerRpc(RequireOwnership = false)]
+    public void SetServerRpc(bool value){
+        m_State.Value = value;
+    }
+
+}
+
