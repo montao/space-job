3b88a35021ae5d1dfa139af2d8e45960b5aee616
fionn-the-goose <ericksoncooperation@gmail.com>
2022-06-04 17:29:06 +0200

    on value change for speed of player

--- Assets/Scripts/Multiplayer/PlayerAnimationController.cs
+++ Assets/Scripts/Multiplayer/PlayerAnimationController.cs
@@ -14,9 +14,25 @@ public static class Animation{
 }
 
 public class PlayerAnimationController : NetworkBehaviour {
-    private Animator animation;
+    private Animator m_PlayerAnimator;
+    private PlayerAvatar m_Player;
+
+    private void Awake() {
+        m_PlayerAnimator = GetComponent<Animator>();
+        m_Player = GetComponent<PlayerAvatar>();
+    }
+
     [ClientRpc]
     public void TriggerAnimationClientRpc(string animation){
-        
+        m_PlayerAnimator.SetTrigger(animation);
+    }
+    
+    public void OnSpeedChange(float prev, float current){
+        m_PlayerAnimator.SetFloat("speed", current);
+    }
+
+    public override void OnNetworkSpawn(){
+        base.OnNetworkSpawn();
+        m_Player.HorizontalSpeed.OnValueChanged += OnSpeedChange;
     }
 }
