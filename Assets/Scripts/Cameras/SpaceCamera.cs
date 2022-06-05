df3f1d1d304afad9631c2fc36b5fcfcb9d7cbcf3
Jakob Ruckel <jakob@ruckel.de>
2022-06-05 19:51:03 +0200

    Fix Space Texture in builds

--- Assets/Scripts/Cameras/SpaceCamera.cs
+++ Assets/Scripts/Cameras/SpaceCamera.cs
@@ -2,10 +2,19 @@ using UnityEngine;
 
 public class SpaceCamera : MonoBehaviour {
 
+    public const int RENDER_TEXTURE_SCALE_DOWN = 2;
+
+    [SerializeField]
+    private Material m_SpaceMaterial;
+
     private Camera m_Camera;
+    private RenderTexture m_RenderTex = null;
 
     void Awake() {
         m_Camera = GetComponent<Camera>();
+        if (!Application.isEditor) {
+            CreateRenderTexture();
+        }
     }
 
     void LateUpdate() {
@@ -16,5 +25,20 @@ public class SpaceCamera : MonoBehaviour {
         }
         transform.rotation = ship_rotation * CameraBrain.Instance.OutputCamera.transform.rotation;
         m_Camera.fieldOfView = CameraBrain.Instance.OutputCamera.fieldOfView;
+    }
+
+    public void CreateRenderTexture() {
+        if (m_RenderTex != null) {
+            // i hope this deletes it?
+            m_RenderTex.Release();
+            m_RenderTex.DiscardContents();
+            m_RenderTex = null;
+        }
+
+        int width = Screen.width / RENDER_TEXTURE_SCALE_DOWN;
+        int height = Screen.height / RENDER_TEXTURE_SCALE_DOWN;
+        m_RenderTex = new RenderTexture(width, height, 0);
+        m_Camera.targetTexture = m_RenderTex;
+        m_SpaceMaterial.mainTexture = m_RenderTex;
     }
 }
