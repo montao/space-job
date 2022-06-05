e0b3334124768a9380a4c7be03db1b0e0a682228
Jakob Ruckel <jakob@ruckel.de>
2022-06-05 21:57:49 +0200

    LightManager: Disable lightmaps during power outage

--- Assets/Scripts/Ship/LightManager.cs
+++ Assets/Scripts/Ship/LightManager.cs
@@ -1,33 +1,47 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LightManager : MonoBehaviour
-{
+public class LightManager : MonoBehaviour {
 
     public List<Light> NormalLights;
     public List<Light> BackupLights;
     public static LightManager Instance;
-    public void SetNormal(bool on){
-        foreach (Light light in NormalLights){
-            light.gameObject.SetActive(on);
-        }
-    }
-    public void SetBackup(bool on){
-        foreach (Light light in BackupLights){
-            light.gameObject.SetActive(on);
-        }
-    }
+
+    private LightmapData[] m_LightmapData;
+    private LightmapData[] m_EmptyLightmapData;
+
     private void Awake() {
         if (Instance == null) {
             Instance = this;
         } else {
             Destroy(this);
         }
+
+        m_LightmapData = LightmapSettings.lightmaps;
+        m_EmptyLightmapData = new LightmapData[]{};
     }
 
     void Start() {
         SetNormal(true);
         SetBackup(false);
     }
+
+    public void SetNormal(bool on){
+        foreach (Light light in NormalLights){
+            light.gameObject.SetActive(on);
+        }
+        SetLightmapsEnabled(true);
+    }
+
+    public void SetBackup(bool on){
+        foreach (Light light in BackupLights){
+            light.gameObject.SetActive(on);
+        }
+        SetLightmapsEnabled(false);
+    }
+
+    private void SetLightmapsEnabled(bool enabled) {
+        LightmapSettings.lightmaps = enabled ? m_LightmapData : m_EmptyLightmapData;
+    }
+
 }
