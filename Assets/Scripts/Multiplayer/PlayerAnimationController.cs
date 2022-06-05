36e77a38497b0c3d0fa504658d96c1ae3d6a787b
Mielein <marie.trojan@uni-weimar.de>
2022-06-05 14:59:14 +0200

    intense gulping has been added

--- Assets/Scripts/Multiplayer/PlayerAnimationController.cs
+++ Assets/Scripts/Multiplayer/PlayerAnimationController.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using Unity.Netcode;
+using System.Collections;
 using System;
 
 public enum PlayerAnimation {
@@ -14,12 +15,20 @@ public enum PlayerAnimation {
 public class PlayerAnimationController : NetworkBehaviour {
     private Animator m_PlayerAnimator;
     private PlayerAvatar m_Player;
-
+    [SerializeField]
+    private AudioClip[] gulpSounds;
+    private AudioSource audioSource;
+    private void Awake() {
+        audioSource = GetComponent<AudioSource>();
+    }
     private void Start() {
         m_PlayerAnimator = GetComponent<Animator>();
         m_Player = GetComponent<PlayerAvatar>();
     }
 
+    private AudioClip GetRandomClip(){
+        return gulpSounds[UnityEngine.Random.Range(0, gulpSounds.Length)];
+    }
     private void TriggerAnimationLocally(PlayerAnimation animation) {
         Debug.Log("Triggering " + animation.ToString().ToLower());
         foreach(PlayerAnimation ani in Enum.GetValues(typeof(PlayerAnimation))){
@@ -60,6 +69,12 @@ public class PlayerAnimationController : NetworkBehaviour {
         m_PlayerAnimator.SetFloat("speed", speed);
     }
 
+    IEnumerator WaitForMouth(float waitTime){
+        yield return new WaitForSeconds(waitTime);
+        AudioClip sound = GetRandomClip();
+        audioSource.PlayOneShot(sound);
+        Debug.Log("gulp");
+    }
     void Update() {
         if (IsOwner) {
             if (Input.GetKeyDown(KeyCode.Alpha1)) {
@@ -70,6 +85,11 @@ public class PlayerAnimationController : NetworkBehaviour {
             }
             if (Input.GetKeyDown(KeyCode.Alpha3)) {
                 TriggerAnimation(PlayerAnimation.DRINK);
+                Debug.Log(m_Player.GetInventoryItem(PlayerAvatar.Slot.PRIMARY).name);
+                if(m_Player.GetInventoryItem(PlayerAvatar.Slot.PRIMARY).tag == "Drink"){
+                    StartCoroutine(WaitForMouth(0.5f));
+                }
+                
             }
             if (Input.GetKeyDown(KeyCode.Alpha4)) {
                 TriggerAnimation(PlayerAnimation.SIT);
