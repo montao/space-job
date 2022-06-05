30dec7d5f15dbfbd7840fff149502de659a1dc0e
goose <ericksoncooperation@gmail.com>
2022-06-05 23:29:39 +0200

    GooseBuddyMesh.cs now for buddy following

--- Assets/Scripts/GooseBuddyNavMesh.cs
+++ Assets/Scripts/GooseBuddyNavMesh.cs
@@ -6,10 +6,12 @@ using UnityEngine.AI;
 public class GooseBuddyNavMesh : MonoBehaviour
 {
     private NavMeshAgent m_GooseNavMesh;
+    private Collider m_FollowingRange;
     public Transform Destination;
     void Start()
     {
         m_GooseNavMesh = GetComponent<NavMeshAgent>();
+        m_FollowingRange = GetComponent<Collider>();
     }
 
     private void SetEvilBehaviour(){
@@ -20,16 +22,18 @@ public class GooseBuddyNavMesh : MonoBehaviour
 
     }
 
-    private void SetIdleBehaviour(){
-
-    }
-
     // idle through ship
     // follow player
     // steal mission (priorieat)
 
+    private void OnCollisionEnter(Collision other) {
+        if (other.gameObject.GetComponent<PlayerAvatar>() != null){
+            Destination = other.gameObject.transform;
+        }
+    }
+
     void Update()
     {
-
+        m_GooseNavMesh.destination = Destination.position;
     }
 }
