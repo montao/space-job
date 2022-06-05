0427013355509a3e497e51d175317fc55e907c82
Mielein <marie.trojan@uni-weimar.de>
2022-06-05 13:37:07 +0200

    cup pickup sound has been added

--- Assets/Scripts/Interactables/CoffeCup.cs
+++ Assets/Scripts/Interactables/CoffeCup.cs
@@ -14,12 +14,17 @@ public class CoffeCup : DroppableInteractable{
     private static int m_CupNumber = 0;
     public List<Material> Materials = new List<Material>();
     private bool m_PickedUp = false;  // Server-Only
+    [SerializeField]
+    private AudioClip[] pickupSounds;
+    private AudioSource audioSource;
+
 
 
     public override void Awake() {
         base.Awake();
         int mat_idx = (m_CupMaterialPattern >> (m_CupNumber++ % 32)) & 1;
         GetComponent<MeshRenderer>().material = Materials[mat_idx];
+        audioSource = GetComponent<AudioSource>();   
     }
 
     public override PlayerAnimation SelfInteraction(PlayerAvatar avatar) {
@@ -27,10 +32,17 @@ public class CoffeCup : DroppableInteractable{
         return PlayerAnimation.DRINK;
     }
 
+    private AudioClip GetRandomClip(){
+        return pickupSounds[UnityEngine.Random.Range(0, pickupSounds.Length)];
+    }
+
     [ServerRpc(RequireOwnership = false)]
     public override void SetHolderServerRpc(int holder_id) {
         base.SetHolderServerRpc(holder_id);
         m_PickedUp = true;
+        AudioClip sound = GetRandomClip();
+        audioSource.PlayOneShot(sound);
+        Debug.Log("Pick Cup");
     }
 
     public bool IsPickedUp() {
