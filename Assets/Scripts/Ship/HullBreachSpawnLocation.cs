d3b7098e973ae20dc0f24b7abf01b4ce3842a999
Jakob Ruckel <jakob@ruckel.de>
2022-06-07 18:03:28 +0200

    Fix nullpointer in HullBreachSpawnLocation

--- Assets/Scripts/Ship/HullBreachSpawnLocation.cs
+++ Assets/Scripts/Ship/HullBreachSpawnLocation.cs
@@ -7,9 +7,11 @@ public class HullBreachSpawnLocation : MonoBehaviour {
     void OnDrawGizmos() {
         var color_prev = Gizmos.color;
         Gizmos.color = Color.cyan;
-        if (GizmoMesh == null || GizmoMeshes.Instance == null) {
+        if (GizmoMesh == null) {
             Gizmos.DrawIcon(transform.position, "HullBreachIcon.png", true);
-            GizmoMesh = GizmoMeshes.Instance.HullBreach;
+            if (GizmoMeshes.Instance != null) {
+                GizmoMesh = GizmoMeshes.Instance.HullBreach;
+            }
         } else {
             Gizmos.DrawWireMesh(GizmoMesh, transform.position, transform.rotation, 2 * Vector3.one);
         }
