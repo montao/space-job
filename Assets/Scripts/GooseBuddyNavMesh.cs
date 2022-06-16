dfce22cf141c86fe2f1b081f8d89d068e45dff43
Jakob Ruckel <jakob@ruckel.de>
2022-06-16 12:22:53 +0200

    Begin some work on Goose ITEM_DEPOSIT

--- Assets/Scripts/GooseBuddyNavMesh.cs
+++ Assets/Scripts/GooseBuddyNavMesh.cs
@@ -13,8 +13,12 @@ public class GooseBuddyNavMesh : MonoBehaviour {
     public Animator GooseAnimator;
     public Animator RadarAnimator;
 
+    public LayerMask GroundLayer;
+    public Transform ItemDepositTarget;  // create an empty child here
+
     public enum GooseState {
         ITEM_GET,
+        ITEM_DEPOSIT,  // called when
         CHASE_PLAYER,
         ASLEEP,
         DANCE,
@@ -22,6 +26,7 @@ public class GooseBuddyNavMesh : MonoBehaviour {
 
     public static GooseState RandomState() {
         int i = UnityEngine.Random.Range(0, Enum.GetValues(typeof(GooseState)).Length);
+        Debug.Log("Goose " + i);
         return (GooseState)Enum.ToObject(typeof(GooseState), i);
     }
     public static Transform RandomTarget(GooseState state) {
@@ -44,11 +49,12 @@ public class GooseBuddyNavMesh : MonoBehaviour {
         return targets[i];
     }
 
-    private GooseState __set_via_properties_State = GooseState.ASLEEP;
+    // only set this via the property, or the goose will die!
+    private GooseState __State = GooseState.ASLEEP;
     public GooseState State {
-        get => __set_via_properties_State;
+        get => __State;
         set {
-            var prev = __set_via_properties_State;
+            var prev = __State;
             if (prev == value) return;
 
             bool awake = value != GooseState.ASLEEP;
@@ -61,12 +67,32 @@ public class GooseBuddyNavMesh : MonoBehaviour {
             } else if (value == GooseState.ITEM_GET || value == GooseState.CHASE_PLAYER) {
                 m_GooseNavMesh.stoppingDistance = (value == GooseState.CHASE_PLAYER) ? 1f : 0f;
                 m_Target = RandomTarget(value);
+            } else if (value == GooseState.ITEM_DEPOSIT) {
+                m_GooseNavMesh.stoppingDistance = 0f;
+                ItemDepositTarget.position = RandomPointOnNavMesh(max_distance: 80);
+                m_Target = ItemDepositTarget;
             }
 
             Debug.Log("Goose is now " + value);
 
-            __set_via_properties_State = value;
+            __State = value;
         }
+    }
+
+    private Vector3 RandomPointOnNavMesh(float max_distance) {
+        if (max_distance < 0) {
+            Debug.LogWarning("No target position found for the Goose :(");
+            return transform.position;
+        }
+
+        Vector3 direction = UnityEngine.Random.insideUnitSphere * max_distance;
+        NavMeshHit hit;
+        if (NavMesh.SamplePosition(transform.position + direction, out hit, max_distance, GroundLayer)) {
+            return hit.position;
+        }
+
+        Debug.Log("No Goose destination found, trying again with maxdist=" + (max_distance - 10) + "...");
+        return RandomPointOnNavMesh(max_distance - 10);
     }
 
     void Start() {
@@ -96,14 +122,21 @@ public class GooseBuddyNavMesh : MonoBehaviour {
         }
         GooseAnimator.SetFloat("Speed", m_GooseNavMesh.velocity.magnitude);
         if (State == GooseState.ITEM_GET && m_GooseNavMesh.remainingDistance < 0.05f) {
-            State = RandomState();
+            State = GooseState.ITEM_DEPOSIT;
         }
     }
 
     private IEnumerator GooseLoop() {
         while (true) {
             State = RandomState();
-            yield return new WaitForSeconds(20f);
+            yield return new WaitForSeconds(5f);
+        }
+    }
+
+    void OnDrawGizmos() {
+        Gizmos.color = Color.yellow;
+        if (m_Target) {
+            Gizmos.DrawCube(m_Target.position, Vector3.one);
         }
     }
 
