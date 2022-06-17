9747a1775cb0a6b49707de036d8971d7b95d8141
Jakob Ruckel <jakob@ruckel.de>
2022-06-17 23:11:54 +0200

    Flashlight: Turn off for everyone on pickup

--- Assets/Scripts/Interactables/FlashLight.cs
+++ Assets/Scripts/Interactables/FlashLight.cs
@@ -1,31 +1,79 @@
 using UnityEngine;
+using Unity.Netcode;
 
 public class FlashLight : DroppableInteractable {
 
     private Light m_Beam;
-    private bool m_TurnedOn = true;
+    private FlashLightInHand m_InHand;
+
+    private NetworkVariable<bool> m_TurnedOn = new NetworkVariable<bool>(true);
+    private NetworkVariable<Quaternion> m_RotationInHand = new NetworkVariable<Quaternion>();
 
     public override void Awake() {
         base.Awake();
         m_Beam = GetComponentInChildren<Light>();
 
-        OnDrop = (PlayerAvatar avatar) => {
-            avatar.GetComponentInChildren<FlashLightInHand>().gameObject.SetActive(false);
-            m_Beam.enabled = true;
-        };
+        // Only run on local client
         OnPickup = (PlayerAvatar avatar) => {
             var fl_hand = avatar.GetComponentInChildren<FlashLightInHand>(includeInactive: true);
             fl_hand.gameObject.SetActive(true);
             fl_hand.ItemRenderer = avatar.PrimaryItemDisplay;
             m_Beam.enabled = false;
         };
+
+        // Only run on local client
+        OnDrop = (PlayerAvatar avatar) => {
+            avatar.GetComponentInChildren<FlashLightInHand>().gameObject.SetActive(false);
+            m_Beam.enabled = true;
+        };
+    }
+
+    public override void OnNetworkSpawn() {
+            base.OnNetworkSpawn();
+            m_State.OnValueChanged += OnHolderChanged;
+            m_TurnedOn.OnValueChanged += OnTurnedOnChanged;
+    }
+
+    public override void OnNetworkDespawn() {
+            base.OnNetworkDespawn();
+            m_State.OnValueChanged -= OnHolderChanged;
+            m_TurnedOn.OnValueChanged -= OnTurnedOnChanged;
+    }
+
+    public void OnHolderChanged(int prev, int curr) {
+        if (curr == DroppableInteractable.IN_WORLD) {  // dropped
+            m_Beam.enabled = true;
+        } else {  // picked up
+            m_Beam.enabled = false;
+        }
+    }
+
+    public void OnRotatationUpdated(Quaternion _, Quaternion rot) {
+        if (LocalClientIsHolding()) {
+            return;
+        }
+    }
+
+    public bool LocalClientIsHolding() {
+        return (int)NetworkManager.Singleton.LocalClientId == m_State.Value;
+    }
+
+    [ServerRpc]
+    public void SetTurnedOnServerRpc(bool turned_on) {
+        // TODO, for now always on
+    }
+
+    public void OnTurnedOnChanged(bool _, bool turned_on) {
+        // TODO
     }
 
     public override PlayerAnimation SelfInteraction(PlayerAvatar avatar) {
+        /* TODO
         m_TurnedOn = !m_TurnedOn;
         m_Beam.enabled = m_TurnedOn;
         AudioClip sound = GetRandomInteractionClip();
         audioSource.PlayOneShot(sound);
+        */
         return PlayerAnimation.INTERACT;
     }
 }
