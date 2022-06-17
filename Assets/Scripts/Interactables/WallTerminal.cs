0e6cbb1442708f2b1d3df877b55130d75571b7eb
goose <ericksoncooperation@gmail.com>
2022-06-18 01:31:30 +0200

    adding doorterminals to all doors over the prfabs

--- Assets/Scripts/Interactables/WallTerminal.cs
+++ Assets/Scripts/Interactables/WallTerminal.cs
@@ -1,22 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class WallTerminal : TwoLevelInteractable
 {
-    private Animator Visor;
+    private Animator m_Visor;
+    public TMP_Text StateDisplay;
     public override void Start() {
         base.Start();
-        Visor = GetComponent<Animator>();
+        m_Visor = GetComponent<Animator>();
         if(m_InteractionRange)
         m_InteractionRange.OnRangeTriggerEnter += OnVisorTriggerEnter;
         m_InteractionRange.OnRangeTriggerExit += OnVisorTriggerExit;
+        StateDisplay.text = ">closed";
+        StateDisplay.color = new Color32(156, 156, 156, 255);
     }
 
     public void OnVisorTriggerEnter(Collider other){
-        Visor.SetBool("active", true);
+        m_Visor.SetBool("active", true);
     }
     public void OnVisorTriggerExit(Collider other){
-        Visor.SetBool("active", false);
+        m_Visor.SetBool("active", false);
+    }
+
+    public void DisplayDoorState(bool should_close){
+        if(should_close){
+            StateDisplay.text = ">close";
+            StateDisplay.color = new Color32(156, 156, 156, 255);
+        }
+        else{
+            StateDisplay.text = ">open";
+            StateDisplay.color = new Color32(33, 169, 33, 255);
+        }
     }
 }
--- Assets/Scripts/Interactables/WallTerminalButton.cs
+++ Assets/Scripts/Interactables/WallTerminalButton.cs
@@ -6,7 +6,9 @@ public class WallTerminalButton : InteractableBase
 {
     public Door ConnectedDoor;
     public bool ShouldClose;
+    public WallTerminal DoorTerminal;
     protected override void Interaction(){
         ConnectedDoor.SetDoorStatusServerRpc(!ShouldClose);
+        DoorTerminal.DisplayDoorState(ShouldClose);
     }
 }
