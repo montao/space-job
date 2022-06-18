2b3183cb3db516fb6eb98f62e8b11c64a09ffe4b
goose <ericksoncooperation@gmail.com>
2022-06-18 13:49:25 +0200

    NumpadButtons in prefab

---
+++ Assets/Scripts/Interactables/NumPadButton.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumPadButton : InteractableBase
+{
+    public int Number;
+        protected override void Interaction(){
+    }
+}
