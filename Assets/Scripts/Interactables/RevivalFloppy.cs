a3e5a5a5b47175736c3335731393ffd9cf22165b
Jakob Ruckel <jakob@ruckel.de>
2022-06-19 00:37:43 +0200

    Despawn only possible on server

--- Assets/Scripts/Interactables/RevivalFloppy.cs
+++ Assets/Scripts/Interactables/RevivalFloppy.cs
@@ -8,14 +8,20 @@ public class RevivalFloppy : DroppableInteractable {
     public override PlayerAnimation SelfInteraction(PlayerAvatar avatar_reviving) {
         Debug.Log("Revival Time!");
         NetworkObject avatar_to_revive_neto;
+        CanInteract = false;
         var pos = avatar_reviving.transform.position + (1.5f * avatar_reviving.transform.forward) + (0.2f * Vector3.up);
         if (Player.Value.TryGet(out avatar_to_revive_neto)) {
             avatar_to_revive_neto.GetComponent<PlayerAvatar>().ReviveServerRpc(pos);
-            GetComponentInParent<NetworkObject>().Despawn(destroy: true);
         } else {
             Debug.LogError("Attempted to revive, but no playeravatar object found.");
         }
 
+        DespawnServerRpc();
         return base.SelfInteraction(avatar_reviving);  // TODO? animation?
+    }
+
+    [ServerRpc(RequireOwnership = false)]
+    public void DespawnServerRpc() {
+        GetComponentInParent<NetworkObject>().Despawn(destroy: true);
     }
 }
