7085df234eaf8ffb8cf4cb4227687fd81f8dfda5
fionn-the-goose <ericksoncooperation@gmail.com>
2022-06-18 17:39:34 +0200

    DeathZone wait

--- Assets/Scripts/DeathZone.cs
+++ Assets/Scripts/DeathZone.cs
@@ -9,11 +9,11 @@ public class DeathZone : MonoBehaviour
     private void Start() {
         DeathField = GetComponent<Collider>();
     }
-
+/* 
     private void OnTriggerStay(Collision other) {
         Debug.Log("hi");
         if (other.gameObject.GetComponent<PlayerAvatar>() != null){
             other.gameObject.GetComponent<PlayerAvatar>().HidePlayer(true);
-        }
-    }
+        } 
+    }*/
 }
