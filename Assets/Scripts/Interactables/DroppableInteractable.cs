e8f9b0c74c056517c7385aa20650fd1f4f70960c
Jakob Ruckel <jakob@ruckel.de>
2022-06-18 13:59:13 +0200

    Cleanup DroppableInteractable script

--- Assets/Scripts/Interactables/DroppableInteractable.cs
+++ Assets/Scripts/Interactables/DroppableInteractable.cs
@@ -4,29 +4,61 @@ using Unity.Netcode;
 using Unity.Netcode.Components;
 
 public abstract class DroppableInteractable : Interactable<int>{
+
+    /**
+     * These four will be disabled when the item is in a player's hand.
+     */
     protected MeshRenderer m_Mesh;
     protected Rigidbody m_Rigidbody;
     protected List<Collider> m_AllCollider;
     protected NetworkTransform m_NetTransform;
+
+    [Tooltip("Velocity with which the item is dropped.")]
     public float Velocity = 100;
+
+    /**
+     * m_State is either the ClientId of the player holding the item,
+     * or this value if the item is not held by anyone.
+     */
     public const int IN_WORLD = -1;
+
+    [Tooltip("Position of PrimaryItemDisplay when Item is held")]
+    public Vector3 PrimaryPos;
+    [Tooltip("Rotation of PrimaryItemDisplay when Item is held")]
+    public Vector3 PrimaryRot;
+    [Tooltip("Position of SecondaryItemDisplay when Item is held")]
+    public Vector3 SecondaryPos;
+    [Tooltip("Rotation of SecondaryItemDisplay when Item is held")]
+    public Vector3 SecondaryRot;
+
+    /** Audio stuff */
     [SerializeField]
     protected AudioClip[] pickupSounds;
     [SerializeField]
     protected AudioClip[] interactionSounds;
     protected AudioSource audioSource;
 
+    /// <summary>
+    /// Delegate for OnPickup and OnDrop events.
+    /// </summary>
     public delegate void OnPickupDropDelegate(PlayerAvatar avatar);
+
+    /// <summary>
+    /// Called on all clients when the Item has been picked up.
+    /// For local client, it is called immediateley (i.e. without waiting for the server).
+    /// See <see cref="Flashlight"/> for how this may be used.
+    /// </summary>
     public event OnPickupDropDelegate OnPickup;
+
+    /// <summary>
+    /// Called on all clients when the Item has been dropped.
+    /// For local client, it is called immediateley (i.e. without waiting for the server).
+    /// See <see cref="Flashlight"/> for how this may be used.
+    /// </summary>
     public event OnPickupDropDelegate OnDrop;
 
-    public Vector3 PrimaryPos;
-    public Vector3 PrimaryRot;
-    public Vector3 SecondaryPos;
-    public Vector3 SecondaryRot;
+    /* ================== LIFECYCLE METHODS ================== */
 
-//----------------------------------------------------------------------------------------------
-    
     public virtual void Awake() {
         m_AllCollider = new List<Collider>(GetComponentsInParent<Collider>());
         m_AllCollider.AddRange(GetComponents<Collider>());
@@ -34,12 +66,6 @@ public abstract class DroppableInteractable : Interactable<int>{
         m_Rigidbody = GetComponentInParent<Rigidbody>();
         m_NetTransform = GetComponentInParent<NetworkTransform>();
         audioSource = GetComponent<AudioSource>(); 
-    }
-    protected AudioClip GetRandomInteractionClip(){
-        return interactionSounds[UnityEngine.Random.Range(0, interactionSounds.Length)];
-    }
-    protected AudioClip GetRandomPickUpClip(){
-        return pickupSounds[UnityEngine.Random.Range(0, pickupSounds.Length)];
     }
 
     public override void OnNetworkSpawn() {
@@ -49,10 +75,30 @@ public abstract class DroppableInteractable : Interactable<int>{
         }
     }
 
+
+    /* ================== LISTENERS & EVENTS ================== */
+
+    public override void OnStateChange(int previous, int current) {
+        if (current != previous) {
+            // inWorld changed, i.e. item was dropped or picked up
+            UpdateWorldstate(current == IN_WORLD);
+        }
+    }
+
+    public void InvokeOnPickup(PlayerAvatar avatar) {
+        OnPickup?.Invoke(avatar);
+    }
+
+    public void InvokeOnDrop(PlayerAvatar avatar) {
+        OnDrop?.Invoke(avatar);
+    }
+
+
+    /* ================== REMOTE PROCEDURES (RPCs) ================== */
+
     [ServerRpc(RequireOwnership = false)]
     public virtual void SetHolderServerRpc(int holder_id){
         m_State.Value = holder_id;
-        
     }
 
     [ServerRpc(RequireOwnership = false)]
@@ -62,6 +108,10 @@ public abstract class DroppableInteractable : Interactable<int>{
         m_Rigidbody.AddForce(Vector3.Normalize(PlayerManager.Instance.LocalPlayer.Avatar.transform.forward) * Velocity);
 
     }
+
+
+    /* ================== INTERACTION ================== */
+
     protected override void Interaction(){
         PlayerAvatar localPlayer = PlayerManager.Instance.LocalPlayer.Avatar;
 
@@ -75,13 +125,6 @@ public abstract class DroppableInteractable : Interactable<int>{
         SetHolderServerRpc((int) NetworkManager.Singleton.LocalClientId);
         AudioClip sound = GetRandomPickUpClip();
         audioSource.PlayOneShot(sound);
-    }
-
-    public override void OnStateChange(int previous, int current) {
-        if (current != previous) {
-            // inWorld changed, i.e. item was dropped or picked up
-            UpdateWorldstate(current == IN_WORLD);
-        }
     }
 
 
@@ -99,11 +142,15 @@ public abstract class DroppableInteractable : Interactable<int>{
         m_NetTransform.enabled = inWorld;
     }
 
-    public void InvokeOnPickup(PlayerAvatar avatar) {
-        OnPickup?.Invoke(avatar);
+
+    /* ================== AUDIO ================== */
+
+    protected AudioClip GetRandomInteractionClip(){
+        return interactionSounds[UnityEngine.Random.Range(0, interactionSounds.Length)];
     }
 
-    public void InvokeOnDrop(PlayerAvatar avatar) {
-        OnDrop?.Invoke(avatar);
+    protected AudioClip GetRandomPickUpClip(){
+        return pickupSounds[UnityEngine.Random.Range(0, pickupSounds.Length)];
     }
+
 }
