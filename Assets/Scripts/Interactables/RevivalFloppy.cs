5b15abcc597b2ac7b21b1db821e62ae8aeb7839b
Jakob Ruckel <jakob@ruckel.de>
2022-06-19 00:17:15 +0200

    Revive *might* already work

--- Assets/Scripts/Interactables/RevivalFloppy.cs
+++ Assets/Scripts/Interactables/RevivalFloppy.cs
@@ -5,8 +5,14 @@ public class RevivalFloppy : DroppableInteractable {
     public NetworkVariable<NetworkObjectReference> Player = new NetworkVariable<NetworkObjectReference>();
 
     // TODO move to medbay
-    public override PlayerAnimation SelfInteraction(PlayerAvatar avatar) {
-        Debug.Log("REVIVE!");
-        return base.SelfInteraction(avatar);
+    public override PlayerAnimation SelfInteraction(PlayerAvatar avatar_reviving) {
+        NetworkObject avatar_to_revive_neto;
+        if (Player.Value.TryGet(out avatar_to_revive_neto)) {
+            avatar_to_revive_neto.GetComponent<PlayerAvatar>().ReviveServerRpc(transform.position);
+        } else {
+            Debug.LogError("Attempted to revive, but no playeravatar object found.");
+        }
+
+        return base.SelfInteraction(avatar_reviving);  // TODO? animation?
     }
 }
