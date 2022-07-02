65418d3890e23869ac6a486017a1441bdb36c821
goose <ericksoncooperation@gmail.com>
2022-07-03 01:16:28 +0200

    fire expands now after spawn

--- Assets/Scripts/Interactables/FireInstance.cs
+++ Assets/Scripts/Interactables/FireInstance.cs
@@ -3,6 +3,7 @@ using Unity.Netcode;
 using System.Collections;
 using System;
 using System.Collections.Generic;
+
 public class FireInstance : RangedInteractableBase
 {
     private float m_DamagePerTick;
@@ -10,11 +11,17 @@ public class FireInstance : RangedInteractableBase
     private Transform m_SpawnLocation;
     private Room m_Room;
     private bool m_IsActive;
+    private float m_MaxSize = 1.5f;
+    private Vector3 m_InitialSize;
+    private Coroutine m_GrowCoroutine;
     public List<FireInstance> FireNeighbours;
+
+
 
     public override void Start() {
         base.Start();
         m_DeathField = GetComponentInChildren<DeathZone>();
+        m_InitialSize = transform.localScale;
     }
 
     protected override void Interaction(){
@@ -27,9 +34,16 @@ public class FireInstance : RangedInteractableBase
         m_Room = room;
         name = "Fire (" + room.name + ")";
         m_SpawnLocation = spawn;
-        //if (IsServer) {
-        //    m_GrowCoroutine = StartCoroutine(Grow());
-        //}
+        if (IsServer) {
+            m_GrowCoroutine = StartCoroutine(Grow());
+        }
+    }
+
+    private IEnumerator Grow() {
+        while(transform.localScale.x < m_MaxSize){
+            yield return new WaitForSeconds(1f);
+            GrowFireClientRpc();
+        }
     }
 
     [ServerRpc(RequireOwnership=false)]
@@ -39,12 +53,8 @@ public class FireInstance : RangedInteractableBase
         GetComponent<NetworkObject>().Despawn(destroy: true);  // breach do be gone
     }
 
-    private void OnTriggerStay(Collider other) {
-        var ava = other.gameObject.GetComponent<PlayerAvatar>();
-        if (ava != null && ava.OwnerClientId == NetworkManager.Singleton.LocalClientId) {
-            if (ava.m_Health.Value > 0) {
-                ava.TakeDamage(0.01f);
-            }
-        }
+    [ClientRpc]
+    public void GrowFireClientRpc() {
+        transform.localScale = 1.1f * transform.localScale;
     }
 }
