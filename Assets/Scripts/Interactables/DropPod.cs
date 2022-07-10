77a62aaa2db1eed472634535825677f2b38f8b50
fionn-the-goose <ericksoncooperation@gmail.com>
2022-07-10 20:06:53 +0200

    drop pod should wp

--- Assets/Scripts/Interactables/DropPod.cs
+++ Assets/Scripts/Interactables/DropPod.cs
@@ -1,7 +1,31 @@
+using UnityEngine;
+using Unity.Netcode;
+
 public class DropPod : RangedInteractableBase {
-    public Door ConnectedDoor;
+    private bool m_CanBeUsed = false;
     protected override void Interaction(){
-        ConnectedDoor.ToogleDoorStatusServerRpc();
+        if (PlayerAvatar.IsHolding<MeetBalls>() && m_CanBeUsed) {
+            FullFillOrderServerRpc();
+        }
+    }
+
+    [ServerRpc(RequireOwnership = false)]
+    public void FullFillOrderServerRpc(){
+        ShipManager.Instance.MarkNearestDestinationAsReached();
+    }
+
+    public override void Update() {
+        base.Update();
+        if(ShipManager.Instance.GetDistantToWin() <= ShipManager.WIN_DISTANCE_THRESHOLD){
+            m_CanBeUsed = true;
+        }
+        else{
+            m_CanBeUsed = false;
+        }
+    }
+
+    public void SetState(bool new_state){
+        m_CanBeUsed = new_state;
     }
 
     public override string FriendlyName() {
