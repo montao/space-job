b4e60149a1cafd1903ea5411961eb42071f3238b
jreeee <jeremias.michael.kilian@uni-weimar.de>
2022-07-11 22:24:59 +0200

    basic item stuff

--- Assets/Scripts/UI/HUD.cs
+++ Assets/Scripts/UI/HUD.cs
@@ -8,24 +8,32 @@ using TMPro;
 public class HUD : NetworkBehaviour
 {
 
-    public Mesh primMesh;
-    public Mesh secMesh;
 
-    public MeshFilter primFilter;
+    public GameObject primObj, secObj;
 
-    public MeshFilter secFilter;
-
+    private Vector3 rotDir;
+    private float speed;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        rotDir = new Vector3(0.0f, 1.0f, 0.0f);
+        speed = 10.0f;
     }
 
     // Update is called once per frame
     void Update()
     {
+        primObj.transform.Rotate(rotDir * Time.deltaTime * speed);
+    }
 
+    public void setMesh(Mesh mesh, bool prim) {
+        if (prim) {
+            primObj.GetComponent<MeshFilter>().mesh = mesh;
+        }
+        else {
+            secObj.GetComponent<MeshFilter>().mesh = mesh;
+        }
     }
 
 }
