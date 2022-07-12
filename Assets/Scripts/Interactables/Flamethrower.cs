f7237624b4ceb8093ba5ab054bc506e98def60c9
fionn-the-goose <ericksoncooperation@gmail.com>
2022-07-12 20:20:27 +0200

    fire extinguisher getting dangerous

--- Assets/Scripts/Interactables/Flamethrower.cs
+++ Assets/Scripts/Interactables/Flamethrower.cs
@@ -2,10 +2,15 @@ using System.Collections.Generic;
 using System.Collections;
 using UnityEngine;
 public class Flamethrower : DroppableInteractable {
+    private Transform m_NearestFire;
     public override PlayerAnimation SelfInteraction(PlayerAvatar avatar) {
         interactionAudioSource.gameObject.transform.position = PlayerManager.Instance.LocalPlayer.Avatar.gameObject.transform.position;
         AudioClip sound = GetRandomInteractionClip();
         interactionAudioSource.PlayOneShot(sound);
+        Room room = PlayerManager.Instance.LocalPlayer.Avatar.CurrentRoom;
+        foreach(Transform fire_location in room.m_FireSpawnLocations){
+            float closest_Fire;
+        }
         return PlayerAnimation.INTERACT;
     }
 
