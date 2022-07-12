ee1bd68306228de34bdbd53a5b30a45cb43d20b8
goose <ericksoncooperation@gmail.com>
2022-07-13 00:34:51 +0200

    Poggers Air Lock

--- Assets/Scripts/Interactables/DropPod.cs
+++ Assets/Scripts/Interactables/DropPod.cs
@@ -2,10 +2,16 @@ using UnityEngine;
 using Unity.Netcode;
 
 public class DropPod : RangedInteractableBase {
-    private bool m_CanBeUsed = false;
+    public bool m_CanBeUsed = false;
+    private Animator doorAnimator;
+    public override void Start(){
+        doorAnimator = GetComponent<Animator>();
+        base.Start();
+    } 
     protected override void Interaction(){
         if (PlayerAvatar.IsHolding<MeetBalls>() && m_CanBeUsed) {
             FullFillOrderServerRpc();
+            PlayerManager.Instance.LocalPlayer.Avatar.GetInventoryItem(PlayerAvatar.Slot.PRIMARY).Despawn();
         }
     }
 
@@ -22,6 +28,8 @@ public class DropPod : RangedInteractableBase {
         else{
             m_CanBeUsed = false;
         }
+        Debug.Log("m_CanBeUsed is: " + m_CanBeUsed);
+        doorAnimator.SetBool("Open", m_CanBeUsed);
     }
 
     public override string FriendlyName() {
