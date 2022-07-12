ed8c037979279016539a419580850f78639094af
Mielein <marie.trojan@uni-weimar.de>
2022-07-12 14:06:38 +0200

    fotobooth buttons dont work yet

--- Assets/DisplayButton.cs
+++ Assets/DisplayButton.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 
 public class DisplayButton : InteractableBase {
+    
     public string buttonName;
     [SerializeField]
     protected FotoboothDisplay display;
--- Assets/FotoboothDisplay.cs
+++ Assets/FotoboothDisplay.cs
@@ -3,9 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class FotoboothDisplay : TwoLevelInteractable {
-    public override void Start() {
-        base.Start();
-    }
+
     public override string FriendlyName() {
         return "Display";
     }
