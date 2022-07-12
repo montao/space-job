9d65306a07baf9fbb5651aca151d0875f7c17204
Mielein <marie.trojan@uni-weimar.de>
2022-07-12 12:16:53 +0200

    staring on character selection

---
+++ Assets/DisplayButton.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class DisplayButton : InteractableBase {
+    public string buttonName;
+    [SerializeField]
+    protected FotoboothDisplay display;
+    protected override void Interaction(){
+        if(buttonName == "Left"){
+            Debug.Log("Left Button");
+        }
+        if(buttonName == "Right"){
+            Debug.Log("Right Button");
+        }
+        if(buttonName == "Confirm"){
+            Debug.Log("Confirm Button");
+        }
+        if(buttonName == "Cancel"){
+            Debug.Log("Cancel Button");
+        }
+    }
+    public override string FriendlyName() {
+        return buttonName;
+    } 
+}
---
+++ Assets/FotoboothDisplay.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FotoboothDisplay : TwoLevelInteractable {
+    public override void Start() {
+        base.Start();
+    }
+    public override string FriendlyName() {
+        return "Display";
+    }
+}
